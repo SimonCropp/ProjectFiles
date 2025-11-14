0599e8cefb268ee3047258c88028f75763a4d317
Simon Cropp <simon.cropp@gmail.com>
2025-11-14 22:16:35 +1100

    .

--- src/ConsumingTests/Tests.cs
+++ src/ConsumingTests/Tests.cs
@@ -1,27 +1,29 @@
+using ProjectFilesGenerator;
+
 [TestFixture]
 public class Tests
 {
     [Test]
     public void Recursive() =>
-        IsTrue(File.Exists(ProjectFiles.RecursiveDirectory.SomeFileTxt));
+        IsTrue(File.Exists(ProjectFiles.RecursiveDirectory.SomeFile_txt));
 
     [Test]
     public void Specific()
     {
-        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.Dir1.File1Txt));
-        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.Dir1.File2Txt));
-        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.Dir2.File4Txt));
-        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.File3Txt));
+        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.Dir1.File1_txt));
+        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.Dir1.File2_txt));
+        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.Dir2.File4_txt));
+        IsTrue(File.Exists(ProjectFiles.SpecificDirectory.File3_txt));
     }
 
     [Test]
     public void Config() =>
-        IsTrue(File.Exists(ProjectFiles.Config.AppsettingsJson));
+        IsTrue(File.Exists(ProjectFiles.Config.Appsettings_json));
 
     [Test]
     public void Nested()
     {
-        IsTrue(File.Exists(ProjectFiles.RecursiveDirectory.SubDir.NestedFileTxt));
-        IsTrue(File.Exists(ProjectFiles.Config.AppsettingsJson));
+        IsTrue(File.Exists(ProjectFiles.RecursiveDirectory.SubDir.NestedFile_txt));
+        IsTrue(File.Exists(ProjectFiles.Config.Appsettings_json));
     }
 }
