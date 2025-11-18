8f07c93ccd50a78bba9326997664a9a3e2ecade2
Cam MacFarland <distantcam@users.noreply.github.com>
2025-11-18 13:08:47 +0800

    Use roslyn to detect keywords (#5)
    Co-authored-by: Cam MacFarland <cameron.macfarland@signifimedia.com.au>

--- src/ProjectFiles/KeywordDetect.cs
+++ src/ProjectFiles/KeywordDetect.cs
@@ -1,90 +1,11 @@
-﻿public static class KeywordDetect
-{
-    static HashSet<string> keywords =
-    [
-        "abstract",
-        "as",
-        "base",
-        "bool",
-        "break",
-        "byte",
-        "case",
-        "catch",
-        "char",
-        "checked",
-        "class",
-        "const",
-        "continue",
-        "decimal",
-        "default",
-        "delegate",
-        "do",
-        "double",
-        "else",
-        "enum",
-        "event",
-        "explicit",
-        "extern",
-        "false",
-        "finally",
-        "fixed",
-        "float",
-        "for",
-        "foreach",
-        "goto",
-        "if",
-        "implicit",
-        "in",
-        "int",
-        "interface",
-        "internal",
-        "is",
-        "lock",
-        "long",
-        "namespace",
-        "new",
-        "null",
-        "object",
-        "operator",
-        "out",
-        "override",
-        "params",
-        "private",
-        "protected",
-        "public",
-        "readonly",
-        "ref",
-        "return",
-        "sbyte",
-        "sealed",
-        "short",
-        "sizeof",
-        "stackalloc",
-        "static",
-        "string",
-        "struct",
-        "switch",
-        "this",
-        "throw",
-        "true",
-        "try",
-        "typeof",
-        "uint",
-        "ulong",
-        "unchecked",
-        "unsafe",
-        "ushort",
-        "using",
-        "virtual",
-        "void",
-        "volatile",
-        "while"
-    ];
+using Microsoft.CodeAnalysis.CSharp;
 
+public static class KeywordDetect
+{
     public static string Sanitize(StringBuilder builder)
     {
         var result = builder.ToString();
-        if (keywords.Contains(result))
+        if (SyntaxFacts.IsKeywordKind(SyntaxFacts.GetKeywordKind(result)))
         {
             return string.Concat("@", result);
         }
