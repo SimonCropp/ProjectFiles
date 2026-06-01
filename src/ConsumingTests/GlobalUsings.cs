9ed521c2c9075b5f37b26c5cfa1ccc2c7ea1fcda
Simon Cropp <simon.cropp@gmail.com>
2026-06-01 21:32:41 +1000

    refs

--- src/ConsumingTests/GlobalUsings.cs
+++ src/ConsumingTests/GlobalUsings.cs
@@ -1,4 +1,2 @@
-﻿// Global using directives
-
-global using System.IO;
+﻿global using System.IO;
 global using NUnit.Framework;
--- src/NugetTests/GlobalUsings.cs
+++ src/NugetTests/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿// Global using directives
-
-global using System.Collections.Generic;
+﻿global using System.Collections.Generic;
 global using System.IO;
 global using NUnit.Framework;
